2dd1cf8dfd1c0a6a7e17395c826aa119438b866b
liuyuuva <liuyus@gmail.com>
2017-06-28 15:03:17 -0400

    save-macro is deleted. useless

--- init.el
+++ init.el
@@ -1698,19 +1698,6 @@ used to fill a paragraph to `my-LaTeX-auto-fill-function'."
 
 (put 'narrow-to-region 'disabled nil)
 
-(defun save-macro (name)
-    "save a macro. Take a name as argument
-     and save the last defined macro under
-     this name at the end of your .emacs"
-     (interactive "Name of the macro :")  ; ask for the name of the macro
-     (kmacro-name-last-macro name)         ; use this name for the macro
-     (find-file user-init-file)            ; open ~/.emacs or other user init file
-     (goto-char (point-max))               ; go to the end of the .emacs
-     (newline)                             ; insert a newline
-     (insert-kbd-macro name)               ; copy the macro
-     (newline)                             ; insert a newline
-     (switch-to-buffer nil))               ; return to the initial buffer
-
 (defun ridm ()
   "Remove intrusive CTRL-Ms from the buffer"
   (interactive)
