e2d8e09162c42746cab4734208489955f55d7866
liuyuuva <liuyus@gmail.com>
2017-10-13 11:51:12 -0400

    fixed company auto menu; made comany work better with yasnippet; turned ace-window backon

--- init.el
+++ init.el
@@ -416,10 +416,9 @@ If SUBMODE is not provided, use `LANG-mode' by default."
 ;;     ))
 
 
-;; (use-package ace-window
-;;   :ensure t
-;;   :defer t
-;;   :config (global-set-key (kbd "M-p") 'ace-window))
+ (use-package ace-window
+   :ensure t
+   :config (global-set-key (kbd "M-p") 'ace-window))
 
 
 (use-package evil
@@ -709,10 +708,10 @@ If SUBMODE is not provided, use `LANG-mode' by default."
   (progn
     (add-hook 'after-init-hook 'global-company-mode)
     
-    (setq company-idle-delay nil
-	  company-show-numbers t
-	  company-async-timeout 50)
-    (setq company-backends
+    ;; (setq company-idle-delay nil
+	;;   company-show-numbers t
+	;;   company-async-timeout 50)
+    (add-to-list 'company-backends
 	  '(company-irony
 		company-gtags
 	    )
@@ -731,8 +730,10 @@ If SUBMODE is not provided, use `LANG-mode' by default."
 
 
 
-(define-key c-mode-map  [(tab)] 'company-complete)
-(define-key c++-mode-map  [(tab)] 'company-complete)
+;(define-key c-mode-map  (kbd "C-<tab>") 'company-complete)
+;(define-key c++-mode-map  (kbd "C-<tab>") 'company-complete)
+;(define-key c-mode-map  [(tab)] 'company-complete)
+;(define-key c++-mode-map  [(tab)] 'company-complete)
 ;;(eval-after-load 'company
 ;;  '(add-to-list 'company-backends 'company-irony))
 (global-set-key (kbd "C-'") 'company-complete-common)
@@ -876,7 +877,6 @@ If SUBMODE is not provided, use `LANG-mode' by default."
 
 
   
-
 
 
 
@@ -1894,3 +1894,98 @@ used to fill a paragraph to `my-LaTeX-auto-fill-function'."
     (goto-char (point-min))
     (replace-string "\C-m\C-j" "\C-j")))
 
+
+  ;; (defun check-expansion ()
+  ;;   (save-excursion
+  ;;     (if (looking-at "\\_>") t
+  ;;       (backward-char 1)
+  ;;       (if (looking-at "\\.") t
+  ;;         (backward-char 1)
+  ;;         (if (looking-at "->") t nil)))))
+
+  ;; (defun do-yas-expand ()
+  ;;   (let ((yas/fallback-behavior 'return-nil))
+  ;;     (yas/expand)))
+
+  ;; (defun tab-indent-or-complete ()
+  ;;   (interactive)
+  ;;   (if (minibufferp)
+  ;;       (minibuffer-complete)
+  ;;     (if (or (not yas/minor-mode)
+  ;;             (null (do-yas-expand)))
+  ;;         (if (check-expansion)
+  ;;             (company-complete-common)
+  ;;           (indent-for-tab-command)))))
+
+  ;; (global-set-key [tab] 'tab-indent-or-complete)
+(defun check-expansion ()
+  (save-excursion
+    (if (looking-at "\\_>") t
+      (backward-char 1)
+      (if (looking-at "\\.") t
+    (backward-char 1)
+    (if (looking-at "->") t nil)))))
+
+(defun do-yas-expand ()
+  (let ((yas/fallback-behavior 'return-nil))
+    (yas/expand)))
+
+(defun tab-indent-or-complete ()
+  (interactive)
+  (cond
+   ((minibufferp)
+    (minibuffer-complete))
+   (t
+    (indent-for-tab-command)
+    (if (or (not yas/minor-mode)
+        (null (do-yas-expand)))
+    (if (check-expansion)
+        (progn
+          (company-manual-begin)
+          (if (null company-candidates)
+          (progn
+            (company-abort)
+            (indent-for-tab-command)))))))))
+
+(defun tab-complete-or-next-field ()
+  (interactive)
+  (if (or (not yas/minor-mode)
+      (null (do-yas-expand)))
+      (if company-candidates
+      (company-complete-selection)
+    (if (check-expansion)
+      (progn
+        (company-manual-begin)
+        (if (null company-candidates)
+        (progn
+          (company-abort)
+          (yas-next-field))))
+      (yas-next-field)))))
+
+(defun expand-snippet-or-complete-selection ()
+  (interactive)
+  (if (or (not yas/minor-mode)
+      (null (do-yas-expand))
+      (company-abort))
+      (company-complete-selection)))
+
+(defun abort-company-or-yas ()
+  (interactive)
+  (if (null company-candidates)
+      (yas-abort-snippet)
+    (company-abort)))
+
+(global-set-key [tab] 'tab-indent-or-complete)
+(global-set-key (kbd "TAB") 'tab-indent-or-complete)
+(global-set-key [(control return)] 'company-complete-common)
+
+(define-key company-active-map [tab] 'expand-snippet-or-complete-selection)
+(define-key company-active-map (kbd "TAB") 'expand-snippet-or-complete-selection)
+
+(define-key yas-minor-mode-map [tab] nil)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+
+(define-key yas-keymap [tab] 'tab-complete-or-next-field)
+(define-key yas-keymap (kbd "TAB") 'tab-complete-or-next-field)
+(define-key yas-keymap [(control tab)] 'yas-next-field)
+(define-key yas-keymap (kbd "C-g") 'abort-company-or-yas)
