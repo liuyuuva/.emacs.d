8992f0b87d11c0d242209c00fbb5591a42d8a8be
liuyuuva <liuyus@gmail.com>
2017-11-15 14:47:36 -0500

    turned on ibuffer, copied aboabo's ibuffer hydra

--- init.el
+++ init.el
@@ -390,7 +390,154 @@ If SUBMODE is not provided, use `LANG-mode' by default."
 	)
   )
 	
-(global-set-key (kbd "C-x C-b") 'buffer-menu) ; this is my preferred buffer list behavior over helm. this is actually what i had been using before helm. not list-buffers in fact, was buffer-menu
+;(global-set-key (kbd "C-x C-b") 'buffer-menu) ; this is my preferred buffer list behavior over helm. this is actually what i had been using before helm. not list-buffers in fact, was buffer-menu
+(global-set-key (kbd "C-x C-b") 'ibuffer); trying ibuffer instead of buffer-menu
+
+(use-package ibuffer
+  :ensure t
+  :config
+  (progn
+	(setq ibuffer-expert t) ; turn off prompt for killing every single time
+	(setq ibuffer-show-empty-filter-groups nil)
+	(define-ibuffer-column size-h
+	  (:name "Size" :inline t)
+	  (cond
+	   ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+	   ((> (buffer-size) 100000) (format "%7.0fk" (/ (buffer-size) 1000.0)))
+	   ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+	   (t (format "%8d" (buffer-size)))))
+	
+	;; Modify the default ibuffer-formats
+	(setq ibuffer-formats
+		  '((mark modified read-only " "
+				  (name 18 18 :left :elide)
+				  " "
+				  (size-h 9 -1 :right)
+				  " "
+				  (mode 16 16 :left :elide)
+				  " "
+				  filename-and-process)))	
+	(setq mp/ibuffer-collapsed-groups (list "Helm" "*Internal*"))
+
+	(defadvice ibuffer (after collapse-helm)
+	  (dolist (group mp/ibuffer-collapsed-groups)
+		(progn
+		  (goto-char 1)
+		  (when (search-forward (concat "[ " group " ]") (point-max) t)
+			(progn
+			  (move-beginning-of-line nil)
+			  (ibuffer-toggle-filter-group)
+			  )
+			)
+		  )
+		)
+	  (goto-char 1)
+	  (search-forward "[ " (point-max) t)
+	  )
+
+	(ad-activate 'ibuffer)
+	)
+  
+  )
+
+
+
+(defhydra hydra-ibuffer-main (:color pink :hint nil)
+  "
+^Mark^         ^Actions^         ^View^          ^Select^              ^Navigation^
+_m_: mark      _D_: delete       _g_: refresh    _q_: quit             _k_:   ↑    _h_
+_u_: unmark    _s_: save marked  _S_: sort       _TAB_: toggle         _RET_: visit
+_*_: specific  _a_: all actions  _/_: filter     _o_: other window     _j_:   ↓    _l_
+_t_: toggle    _._: toggle hydra _H_: help       C-o other win no-select
+"
+  ("m" ibuffer-mark-forward)
+  ("u" ibuffer-unmark-forward)
+  ("*" hydra-ibuffer-mark/body :color blue)
+  ("t" ibuffer-toggle-marks)
+
+  ("D" ibuffer-do-delete)
+  ("s" ibuffer-do-save)
+  ("a" hydra-ibuffer-action/body :color blue)
+
+  ("g" ibuffer-update)
+  ("S" hydra-ibuffer-sort/body :color blue)
+  ("/" hydra-ibuffer-filter/body :color blue)
+  ("H" describe-mode :color blue)
+
+  ("h" ibuffer-backward-filter-group)
+  ("k" ibuffer-backward-line)
+  ("l" ibuffer-forward-filter-group)
+  ("j" ibuffer-forward-line)
+  ("RET" ibuffer-visit-buffer :color blue)
+
+  ("TAB" ibuffer-toggle-filter-group)
+
+  ("o" ibuffer-visit-buffer-other-window :color blue)
+  ("q" quit-window :color blue)
+  ("." nil :color blue))
+
+(defhydra hydra-ibuffer-mark (:color teal :columns 5
+                              :after-exit (hydra-ibuffer-main/body))
+  "Mark"
+  ("*" ibuffer-unmark-all "unmark all")
+  ("M" ibuffer-mark-by-mode "mode")
+  ("m" ibuffer-mark-modified-buffers "modified")
+  ("u" ibuffer-mark-unsaved-buffers "unsaved")
+  ("s" ibuffer-mark-special-buffers "special")
+  ("r" ibuffer-mark-read-only-buffers "read-only")
+  ("/" ibuffer-mark-dired-buffers "dired")
+  ("e" ibuffer-mark-dissociated-buffers "dissociated")
+  ("h" ibuffer-mark-help-buffers "help")
+  ("z" ibuffer-mark-compressed-file-buffers "compressed")
+  ("b" hydra-ibuffer-main/body "back" :color blue))
+
+(defhydra hydra-ibuffer-action (:color teal :columns 4
+                                :after-exit
+                                (if (eq major-mode 'ibuffer-mode)
+                                    (hydra-ibuffer-main/body)))
+  "Action"
+  ("A" ibuffer-do-view "view")
+  ("E" ibuffer-do-eval "eval")
+  ("F" ibuffer-do-shell-command-file "shell-command-file")
+  ("I" ibuffer-do-query-replace-regexp "query-replace-regexp")
+  ("H" ibuffer-do-view-other-frame "view-other-frame")
+  ("N" ibuffer-do-shell-command-pipe-replace "shell-cmd-pipe-replace")
+  ("M" ibuffer-do-toggle-modified "toggle-modified")
+  ("O" ibuffer-do-occur "occur")
+  ("P" ibuffer-do-print "print")
+  ("Q" ibuffer-do-query-replace "query-replace")
+  ("R" ibuffer-do-rename-uniquely "rename-uniquely")
+  ("T" ibuffer-do-toggle-read-only "toggle-read-only")
+  ("U" ibuffer-do-replace-regexp "replace-regexp")
+  ("V" ibuffer-do-revert "revert")
+  ("W" ibuffer-do-view-and-eval "view-and-eval")
+  ("X" ibuffer-do-shell-command-pipe "shell-command-pipe")
+  ("b" nil "back"))
+
+(defhydra hydra-ibuffer-sort (:color amaranth :columns 3)
+  "Sort"
+  ("i" ibuffer-invert-sorting "invert")
+  ("a" ibuffer-do-sort-by-alphabetic "alphabetic")
+  ("v" ibuffer-do-sort-by-recency "recently used")
+  ("s" ibuffer-do-sort-by-size "size")
+  ("f" ibuffer-do-sort-by-filename/process "filename")
+  ("m" ibuffer-do-sort-by-major-mode "mode")
+  ("b" hydra-ibuffer-main/body "back" :color blue))
+
+(defhydra hydra-ibuffer-filter (:color amaranth :columns 4)
+  "Filter"
+  ("m" ibuffer-filter-by-used-mode "mode")
+  ("M" ibuffer-filter-by-derived-mode "derived mode")
+  ("n" ibuffer-filter-by-name "name")
+  ("c" ibuffer-filter-by-content "content")
+  ("e" ibuffer-filter-by-predicate "predicate")
+  ("f" ibuffer-filter-by-filename "filename")
+  (">" ibuffer-filter-by-size-gt "size")
+  ("<" ibuffer-filter-by-size-lt "size")
+  ("/" ibuffer-filter-disable "disable")
+  ("b" hydra-ibuffer-main/body "back" :color blue))
+
+(define-key ibuffer-mode-map "." 'hydra-ibuffer-main/body)
 
 (defun whack-whitespace (arg)
       "Delete all white space from point to the next word.  With prefix ARG
