893ab3c3af1d9c9e6d47f63319c799284bb33c3c
liuyuuva <liuyus@gmail.com>
2017-11-20 10:53:45 -0500

    changed smartparens and breadcrumb hydras

--- init.el
+++ init.el
@@ -20,7 +20,6 @@
 
 		)
   )
-  
 
 (if (eq system-type 'darwin)
   (progn
@@ -794,11 +793,12 @@ _t_: toggle    _._: toggle hydra _H_: help       C-o other win no-select
   (defhydra hydra-breadcrumb (:color red :columns 4)
 	"Breadcrumb"
 	("s" bc-set "Set")
-	("p" bc-previous "Prev")
-	("n" bc-next "Next")
-	("<up>" bc-local-previous "Local Prev")
-	("<down>" bc-local-next "Local Next")
+	("<up>" bc-previous "Prev")
+	("<down>" bc-next "Next")
+	("S-<up>" bc-local-previous "Local Prev")
+	("S-<down>" bc-local-next "Local Next")
 	("l" bc-list "List")
+	("c" bc-clear "Clear")
 	("q" nil "quit")
 	)
   )
@@ -1275,7 +1275,7 @@ _t_: toggle    _._: toggle hydra _H_: help       C-o other win no-select
 (defhydra hydra-semantic-fa (:color blue :columns 4)
   "Semantic and FunctionArgs"
   ("c" semantic-ia-describe-class "descibe class")
-  ("g" semantic-ia-fast-jump "fast jump")
+  ("u" semantic-ia-fast-jump "fast jump")
   ("t" semantic-ia-complete-tip "tip")
   ("m" semantic-ia-complete-symbol-menu "symbol menu")
   ("s" semantic-ia-show-summary "show summary")
@@ -1289,8 +1289,11 @@ _t_: toggle    _._: toggle hydra _H_: help       C-o other win no-select
   ("r" moo-propose-override "moo override")
   ("p" moo-jump-local "moo local jump")
   ("M-l" flycheck-list-errors "flycheck error list")
+  ("M-g" goto-line "go to line")
+  ("g" goto-line "go to line")
   ("q" nil "quit")
   )
+
 
 (use-package hideshow
   :bind
@@ -2094,30 +2097,123 @@ used to fill a paragraph to `my-LaTeX-auto-fill-function'."
   :bind
   (
    ("M-K" . hydra-smartparens/body)
+   ("C-(" . hydra-smartparens/body)
    )
   :config
-  (defhydra hydra-smartparens (:color red :columns 6)
-	"Smartparens"
-	("a" sp-beginning-of-sexp "Beginning")
-	("e" sp-end-of-sexp "End")
-	("<down>" sp-down-sexp "Down")
-	("<up>" sp-up-sexp "Up")
-	("M-<down>" sp-backward-down-sexp "Backward down")
-	("M-<up>" sp-backward-up-sexp "Backward up")
-	("f" sp-foward-sexp "Forward")
-	("b" sp-backward-sexp "Backward")
-	("n" sp-next-sexp "Next")
-	("p" sp-previous-sexp "Prev")
-	("C-f" sp-forward-symbol "Forward symbol")
-	("C-b" sp-backward-symbol "Back symbol")
-	("C-<right>" sp-forward-slurp-sexp "Forward slurp")
-	("M-<right>" sp-forward-barf-sexp "Forward barf")
-	("C-<left>"  sp-backward-slurp-sexp "Backward slurp")
-	("M-<left>"  sp-backward-barf-sexp "Backward barf")
-	("t" sp-transpose-sexp "Transpose")
-	("k" sp-kill-sexp "Kill")
-	("w" sp-copy-sexp "Copy")
-	)
+  (defhydra hydra-smartparens (:color red :hint nil)
+  "
+  _B_ backward-sexp            -----
+  _F_ forward-sexp               _s_ splice-sexp
+  _L_ backward-down-sexp         _df_ splice-sexp-killing-forward
+  _H_ backward-up-sexp           _db_ splice-sexp-killing-backward
+^^------                         _da_ splice-sexp-killing-around
+  _k_ down-sexp                -----
+  _j_ up-sexp                    _C-s_ select-next-thing-exchange
+-^^-----                         _C-p_ select-previous-thing
+  _n_ next-sexp                  _C-n_ select-next-thing
+  _p_ previous-sexp            -----
+  _a_ beginning-of-sexp          _C-f_ forward-symbol
+  _z_ end-of-sexp                _C-b_ backward-symbol
+--^^-                          -----
+  _t_ transpose-sexp             _c_ convolute-sexp
+-^^--                            _g_ absorb-sexp
+  _x_ delete-char                _q_ emit-sexp
+  _dw_ kill-word               -----
+  _dd_ kill-sexp                 _,b_ extract-before-sexp
+-^^--                            _,a_ extract-after-sexp
+  _S_ unwrap-sexp              -----
+-^^--                            _AP_ add-to-previous-sexp
+  _C-h_ forward-slurp-sexp       _AN_ add-to-next-sexp
+  _C-l_ forward-barf-sexp      -----
+  _C-S-h_ backward-slurp-sexp    _ join-sexp
+  _C-S-l_ backward-barf-sexp     _|_ split-sexp
+"
+  ;; TODO: Use () and [] - + * | <space>
+  ("B" sp-backward-sexp );; similiar to VIM b
+  ("F" sp-forward-sexp );; similar to VIM f
+  ;;
+  ("L" sp-backward-down-sexp )
+  ("H" sp-backward-up-sexp )
+  ;;
+  ("k" sp-down-sexp ) ; root - towards the root
+  ("j" sp-up-sexp )
+  ;;
+  ("n" sp-next-sexp )
+  ("p" sp-previous-sexp )
+  ;; a..z
+  ("a" sp-beginning-of-sexp )
+  ("z" sp-end-of-sexp )
+  ;;
+  ("t" sp-transpose-sexp )
+  ;;
+  ("x" sp-delete-char )
+  ("dw" sp-kill-word )
+  ;;("ds" sp-kill-symbol ) ;; Prefer kill-sexp
+  ("dd" sp-kill-sexp )
+  ;;("yy" sp-copy-sexp ) ;; Don't like it. Pref visual selection
+  ;;
+  ("S" sp-unwrap-sexp ) ;; Strip!
+  ;;("wh" sp-backward-unwrap-sexp ) ;; Too similar to above
+  ;;
+  ("C-h" sp-forward-slurp-sexp )
+  ("C-l" sp-forward-barf-sexp )
+  ("C-S-h" sp-backward-slurp-sexp )
+  ("C-S-l" sp-backward-barf-sexp )
+  ;;
+  ;;("C-[" (bind (sp-wrap-with-pair "[")) ) ;;FIXME
+  ;;("C-(" (bind (sp-wrap-with-pair "(")) )
+  ;;
+  ("s" sp-splice-sexp )
+  ("df" sp-splice-sexp-killing-forward )
+  ("db" sp-splice-sexp-killing-backward )
+  ("da" sp-splice-sexp-killing-around )
+  ;;
+  ("C-s" sp-select-next-thing-exchange )
+  ("C-p" sp-select-previous-thing )
+  ("C-n" sp-select-next-thing )
+  ;;
+  ("C-f" sp-forward-symbol )
+  ("C-b" sp-backward-symbol )
+  ;;
+  ;;("C-t" sp-prefix-tag-object)
+  ;;("H-p" sp-prefix-pair-object)
+  ("c" sp-convolute-sexp )
+  ("g" sp-absorb-sexp )
+  ("q" sp-emit-sexp )
+  ;;
+  (",b" sp-extract-before-sexp )
+  (",a" sp-extract-after-sexp )
+  ;;
+  ("AP" sp-add-to-previous-sexp );; Difference to slurp?
+  ("AN" sp-add-to-next-sexp )
+  ;;
+  ("_" sp-join-sexp ) ;;Good
+  ("|" sp-split-sexp )) 
+  ;; (defhydra hydra-smartparens (:color red :columns 6)
+  ;; 	"Smartparens"
+  ;; 	("a" sp-beginning-of-sexp "Beginning")
+  ;; 	("e" sp-end-of-sexp "End")
+  ;; 	("<down>" sp-down-sexp "Down")
+  ;; 	("<up>" sp-up-sexp "Up")
+  ;; 	("M-<down>" sp-backward-down-sexp "Backward down")
+  ;; 	("M-<up>" sp-backward-up-sexp "Backward up")
+  ;; 	("f" sp-foward-sexp "Forward")
+  ;; 	("b" sp-backward-sexp "Backward")
+  ;; 	("n" sp-next-sexp "Next")
+  ;; 	("p" sp-previous-sexp "Prev")
+  ;; 	("C-f" sp-forward-symbol "Forward symbol")
+  ;; 	("C-b" sp-backward-symbol "Back symbol")
+  ;; 	("C-<right>" sp-forward-slurp-sexp "Forward slurp")
+  ;; 	("M-<right>" sp-forward-barf-sexp "Forward barf")
+  ;; 	("C-<left>"  sp-backward-slurp-sexp "Backward slurp")
+  ;; 	("M-<left>"  sp-backward-barf-sexp "Backward barf")
+  ;; 	("C-/" sp-splice-sexp "Splice the wrap")
+  ;; 	("t" sp-transpose-sexp "Transpose")
+  ;; 	("k" sp-kill-sexp "Kill")
+  ;; 	("w" sp-copy-sexp "Copy")
+  ;; 	("C-r" sp-rewrap-sexp "Rewrap")
+  ;; 	("q" nil "Quit")
+  ;; 	)
   )
 
 (global-unset-key (kbd "C-x <left>"))
@@ -2377,7 +2473,7 @@ used to fill a paragraph to `my-LaTeX-auto-fill-function'."
 ;; F10
 ;;;;;;;;
 
-(global-set-key (kbd "<f10>") 'nolinum)
+(global-set-key (kbd "C-c n") 'nolinum)
 
 ;;;;;;;;
 ;; F11
