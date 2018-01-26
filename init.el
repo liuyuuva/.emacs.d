97db0f52b31336a437c1604093aa03c6e59e24c9
Yu Liu <liuyus@gmail.com>
2018-01-26 09:54:33 -0500

    added pdf-tools for linux

--- init.el
+++ init.el
@@ -73,6 +73,7 @@
 	(setq my_org_meeting_notes_file "~/Dropbox/org/personal_meeting_notes.org")
 	(setq org_directory "~/Dropbox/org/")
 	(setq my_ipython_path "~/anaconda3/bin/ipython")
+	(pdf-tools-install)
     )
   )
 
